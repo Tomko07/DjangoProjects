45f5a966514375660eade51a017ea959d8aca791
Tomko07 <Tomko07@users.noreply.github.com>
2016-01-29 22:10:25 +0000

    Completed Book Chap 5

--- BookProject/tango_with_django_project/tango_with_django_project/settings.py
+++ BookProject/tango_with_django_project/tango_with_django_project/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -64,6 +68,7 @@ DATABASES = {
     }
 }
 
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
@@ -77,11 +82,26 @@ USE_L10N = True
 
 USE_TZ = True
 
+
 # Templates
-TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
-TEMPLATE_DIRS = (TEMPLATE_PATH)
+
+TEMPLATE_DIRS = (
+    TEMPLATE_PATH,
+)
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
+
+
+# Media
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
